a324ef2180ba0381d6b422694d93e48b270c2f22
Barron Stone <7518212+barronstone@users.noreply.github.com>
2022-08-13 23:01:44 +0000

    Replaced unused variable with _

--- src/09 Simulate Dice/roll_dice.py
+++ src/09 Simulate Dice/roll_dice.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 def roll_dice(*dice, num_trials=1_000_000):
     counts = Counter()
-    for roll in range(num_trials):
+    for _ in range(num_trials):
         counts[sum((randint(1, sides) for sides in dice))] += 1
 
     print('\nOUTCOME\tPROBABILITY')

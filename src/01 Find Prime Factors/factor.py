c378e63ff39db59eb8b5aa21b093d085c23f1c4d
Barron Stone <7518212+barronstone@users.noreply.github.com>
2022-08-14 16:04:31 +0000

    Changed variable num to number

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -1,10 +1,10 @@
-def get_prime_factors(num):
+def get_prime_factors(number):
     factors = list()
     divisor = 2
-    while divisor <= num:
-        if num % divisor == 0:
+    while divisor <= number:
+        if number % divisor == 0:
             factors.append(divisor)
-            num = num // divisor
+            number = number // divisor
         else:
             divisor += 1
     return factors
